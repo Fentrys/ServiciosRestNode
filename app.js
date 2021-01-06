bf18b755ccad736920610fccac3840506d73bcf2
Bastiaans <luismanuelpd@gmail.com>
2021-01-06 13:45:28 -0400

    Configurar json

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const express = require("express");
 
 var app = express();
 
+const places = [
+    {
+        "title":"Oficina Codigo Fcilito",
+        "description":"Lorem Ipsum",
+        "address":"Lorem Ipsum"
+    },
+    {
+        "title":"Oficina Codigo Fcilito",
+        "description":"Lorem Ipsum",
+        "address":"Lorem Ipsum"
+    },
+    {
+        "title":"Oficina Codigo Fcilito",
+        "description":"Lorem Ipsum",
+        "address":"Lorem Ipsum"
+    },
+    {
+        "title":"Oficina Codigo Fcilito",
+        "description":"Lorem Ipsum",
+        "address":"Lorem Ipsum"
+    }
+]
+
 app.use(express.static("public"));
 
-app.get("/", (req,res) =>{
-    res.send("Hola mundo");
+app.get("/", (req,res) =>{    
+    res.json(places)
 })
 
 app.listen(3000, function(){
